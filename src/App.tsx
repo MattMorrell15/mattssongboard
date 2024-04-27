b4c2440f192bc982300f65ca706ea27953b6200c
MattMorrell15 <morrel18@msu.edu>
2024-04-26 21:47:48 -0400

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,8 @@
-import { useState } from 'react';
+import { } from 'react';
 import lasersharkpic from './assets/lasershark.jpg';
 import lazersharkprofilepic from './assets/lazersharkprofilepic.jpg';
 import './App.css';
 import { createClient } from 'https://esm.sh/@supabase/supabase-js';
-
 const supabaseUrl = 'https://jxhnpukxnjlmtboemcgv.supabase.co';
 const supabaseKey =
   'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imp4aG5wdWt4bmpsbXRib2VtY2d2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTQxNjc0MTEsImV4cCI6MjAyOTc0MzQxMX0.ssx_WjyvnlAW-lMScnX_P3qUBBfYkVShD5UZbqSM0AQ';
